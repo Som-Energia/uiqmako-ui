c399c3661cab0eea10103c189e96ae466e9e5546
Oriol Piera <oriol.piera@somenergia.coop>
2022-06-16 18:03:44 +0200

    Update src/components/SimpleSnackbar.js
    Co-authored-by: MarJene <57089889+MarJene@users.noreply.github.com>

--- src/components/SimpleSnackbar.js
+++ src/components/SimpleSnackbar.js
@@ -22,7 +22,7 @@ function SimpleSnackbar(props) {
           vertical: 'bottom',
           horizontal: 'right',
         }}
-        open={console.log(alertInfo?.open) && alertInfo?.open}
+        open={alertInfo?.open}
         onClose={handleClose}
         message={alertInfo?.message}
         autoHideDuration={3000}
