b7f30d691b3d02d60d24973307dc8f5d8902d4f8
pauboixsom <pau.boix@somenergia.coop>
2022-08-23 12:26:17 +0200

    unusable

--- src/components/RichTextEditor.js
+++ src/components/RichTextEditor.js
@@ -205,7 +205,8 @@ function RichTextEditor(props) {
 
   const editor = useEditor({
     extensions: [StarterKit],
-    content: '<p>Hello World!</p>',
+    content:
+      '<head><meta charset="utf-8" /><table width="100%" frame="below" bgcolor="#E8F1D4"><tr><td height = 2px><FONT SIZE=2><strong>Contracte Som Energia nº ${object.polissa_id.name}:</strong></font></td><td VALIGN=TOP rowspan="4"><align="right"><align="right"><img width="130" height="65" src="https://www.somenergia.coop/wp-content/uploads/2014/11/logo-somenergia.png"></td></tr><tr><td height = 2px><FONT SIZE=1>Adreça punt subministrament: ${object.cups_id.direccio}</font></td></tr><tr><td height = 2px><FONT SIZE=1>Codi CUPS: ${object.cups_id.name}</font></td></tr><tr><td height = 2px width=100%><FONT SIZE=1>Titular: ${object.polissa_id.titular.name}</font></td></tr></table></head>',
   })
 
   useEffect(() => {
@@ -222,7 +223,11 @@ function RichTextEditor(props) {
       {modifiedTexts?.length > 0 &&
         modifiedTexts?.map(
           (item, index) =>
-            (item[0] === 'html' && <MenuBar editor={editor} />) || (
+            (item[0] === 'html' && (
+              <>
+                <MenuBar editor={editor} /> <EditorContent editor={editor} />
+              </>
+            )) || (
               <TextareaAutosize
                 id={index}
                 key={index}
